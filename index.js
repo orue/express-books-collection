ad16e069884dc556600cca50ba8780db47eee19d
Carlos Orue <carlos@orue.io>
2025-06-18 21:09:06 -0500

    change the prettier configuration

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
-const port = 3000
-app.use(express.json())
+const app = express();
+const port = 3000;
+app.use(express.json());
 
-let booksData = []
-let nextId = 1
+let booksData = [];
+let nextId = 1;
 
 // Basic route to check if the server is running
-app.get('/', (req, res) => {
-  res.send({ message: 'hello express' })
-})
+app.get("/", (req, res) => {
+  res.send({ message: "hello express" });
+});
 
 // get all books
-app.get('/books', (req, res) => {
-  res.status(200).send(booksData)
-})
+app.get("/books", (req, res) => {
+  res.status(200).send(booksData);
+});
 
 // get a book by id
-app.get('/books/:id', (req, res) => {
-  const book = booksData.find(b => b.id === parseInt(req.params.id))
+app.get("/books/:id", (req, res) => {
+  const book = booksData.find((b) => b.id === parseInt(req.params.id));
   if (!book) {
-    return res.status(404).send('Book not found')
+    return res.status(404).send("Book not found");
   }
-  res.status(200).send(book)
-})
+  res.status(200).send(book);
+});
 
 // create a new book
-app.post('/book', (req, res) => {
-  const { name, price } = req.body
-  const newBook = { id: nextId++, name, price }
-  booksData.push(newBook)
-  res.status(201).send(newBook)
-})
+app.post("/book", (req, res) => {
+  const { name, price } = req.body;
+  const newBook = { id: nextId++, name, price };
+  booksData.push(newBook);
+  res.status(201).send(newBook);
+});
 
 // update a book by id
-app.put('/books/:id', (req, res) => {
-  const book = booksData.find(b => b.id === parseInt(req.params.id))
+app.put("/books/:id", (req, res) => {
+  const book = booksData.find((b) => b.id === parseInt(req.params.id));
 
   if (!book) {
-    return res.status(404).send('Book not found')
+    return res.status(404).send("Book not found");
   }
-  const { name, price } = req.body
-  book.name = name
-  book.price = price
-  res.status(200).send(book)
-})
+  const { name, price } = req.body;
+  book.name = name;
+  book.price = price;
+  res.status(200).send(book);
+});
 
 // delete a book by id
-app.delete('/books/:id', (req, res) => {
-  const index = booksData.findIndex(b => b.id === parseInt(req.params.id))
+app.delete("/books/:id", (req, res) => {
+  const index = booksData.findIndex((b) => b.id === parseInt(req.params.id));
 
   if (index === -1) {
-    return res.status(404).send('Book not found')
+    return res.status(404).send("Book not found");
   }
-  booksData.slice(index, 1)
-  return res.status(204).send('Deleted')
-})
+  booksData.slice(index, 1);
+  return res.status(204).send("Deleted");
+});
 
 // server listen on port 3000
 app.listen(port, () => {
-  console.log(`Server is running at port: ${port}...`)
-})
+  console.log(`Server is running at port: ${port}...`);
+});
